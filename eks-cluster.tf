227205783a27beece7f6d1cafba2bdcae2960675
ricardopereira87 <1190990@isep.ipp.pt>
2022-05-12 14:42:32 +0100

    changes on the cluster name

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,7 +1,7 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   version         = "17.24.0"
-  cluster_name    = "MyFirstEKS_Teste"
+  cluster_name    = local.cluster_name
   cluster_version = "1.22"
   subnets         = module.vpc.private_subnets
 
