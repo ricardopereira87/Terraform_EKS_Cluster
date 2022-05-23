a5dd3bb576d6705af99b0ea1980dd68ad48cb763
Ricardo Pereira <1190990@isep.ipp.pt>
2022-05-23 16:57:08 +0100

    keyupdated

--- Calico_EKS_Cluster/eks-cluster.tf
+++ Calico_EKS_Cluster/eks-cluster.tf
@@ -19,7 +19,7 @@ module "eks" {
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
       asg_desired_capacity          = 3
       bootstrap_extra_args          = "--use-max-pods false"
-      key_name                      = "KeyPairEKSTrafficAnalizer"
+      key_name                      = "internalAccess_EKS_Tests"
     },
   ]
 }
